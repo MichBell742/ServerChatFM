29735f39a7e26c3b5bc5947b537ab3960c322315
Casagrande <casagrande@air02.local>
2025-10-15 12:44:49 +0200

    dai che ncesemo

--- Server.py
+++ Server.py
@@ -3,8 +3,19 @@ import websockets
 
 async def echo(websocket):
     async for message in websocket:
-        await websocket.send(message)
+        print(message)
+        richiesta = message.split("|")
+        idRichiesta = richiesta[0]
+        if idRichiesta.equals("A"):
+            accesso()
+        elif  idRichiesta.equals("M"):
+            messagio()
+        else:
+            break
+        nome = richiesta[1]
 
+
+            
 async def main():
     async with websockets.serve(echo, "", 8765):
         await asyncio.Future()
